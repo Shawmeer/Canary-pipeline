ca7b7d29964982f7b4beb4d48f96e2ad6e6b03a7
shawmeer <snasri898@gmail.com>
2026-03-26 15:24:13 +0545

    skms

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,13 @@ resource "aws_security_group" "main" {
     } 
 
       ingress{
+        from_port = 3000
+        to_port   = 3000
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }                                
+
+      ingress{
         from_port = 443
         to_port   = 443
         protocol  = "tcp"
